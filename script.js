567da564d5a978decf90aa05b4dbaf9aff380609
Marsid Zyberi <39383860+marsidi11@users.noreply.github.com>
2019-02-27 22:48:30 +0100

    winon()

--- script.js
+++ script.js
@@ -1,3 +1,19 @@
+function winon() {
+	var bs1 = document.getElementById('img1').src;
+	var bs2 = document.getElementById('img2').src;
+	var bs3 = document.getElementById('img3').src;
+	var bs4 = document.getElementById('img4').src;
+	var bs5 = document.getElementById('img5').src;
+	var bs6 = document.getElementById('img6').src;
+	var bs7 = document.getElementById('img7').src;
+	var bs8 = document.getElementById('img8').src;
+	var bs9 = document.getElementById('img9').src;
+	var mo = "img/oicon.svg";
+	var mx = "img/xicon.svg";
+
+	
+}
+
 function computer() {
 
 	if (document.getElementById('img7').src == "data:image/gif;base64,R0lGODlhAQABAAD/ACwAAAAAAQABAAACADs") {
@@ -32,21 +48,22 @@ function computer() {
 function po1() {
 	document.getElementById('img1').src = "img/oicon.svg";
 	computer();
-	check();
+	winon();
 }
 function po2() {
 	document.getElementById('img2').src = "img/oicon.svg";
 	computer();
-	check();
+	winon();
 }
 function po3() {
 	document.getElementById('img3').src = "img/oicon.svg";
 	computer();
-	check();
+	winon();
 }
 function po4() {
 	document.getElementById('img4').src = "img/oicon.svg";
 	computer();
+	winon();
 }
 function po5() {
 	document.getElementById('img5').src = "img/oicon.svg";
@@ -55,48 +72,52 @@ function po5() {
 function po6() {
 	document.getElementById('img6').src = "img/oicon.svg";
 	computer();
+	winon();
 }
 function po7() {
 	document.getElementById('img7').src = "img/oicon.svg";
 	computer();
+	winon();
 }
 function po8() {
 	document.getElementById('img8').src = "img/oicon.svg";
 	computer();
+	winon();
 }
 function po9() {
 	document.getElementById('img9').src = "img/oicon.svg";
 	computer();
+	winon();
 }
 
 
 
 
 function check1() {
-	document.getElementById('img1').src = "img/xicon.svg"
+	document.getElementById('img1').src = "img/xicon.svg";
 }
 function check2() {
-	document.getElementById('img2').src = "img/xicon.svg"
+	document.getElementById('img2').src = "img/xicon.svg";
 }
 function check3() {
-	document.getElementById('img3').src = "img/xicon.svg"
+	document.getElementById('img3').src = "img/xicon.svg";
 }
 function check4() {
-	document.getElementById('img4').src = "img/xicon.svg"
+	document.getElementById('img4').src = "img/xicon.svg";
 }
 function check5() {
-	document.getElementById('img5').src = "img/xicon.svg"
+	document.getElementById('img5').src = "img/xicon.svg";
 }
 function check6() {
-	document.getElementById('img6').src = "img/xicon.svg"
+	document.getElementById('img6').src = "img/xicon.svg";
 }
 function check7() {
-	document.getElementById('img7').src = "img/xicon.svg"
+	document.getElementById('img7').src = "img/xicon.svg";
 }
 function check8() {
-	document.getElementById('img8').src = "img/xicon.svg"
+	document.getElementById('img8').src = "img/xicon.svg";
 }
 function check9() {
-	document.getElementById('img9').src = "img/xicon.svg"
+	document.getElementById('img9').src = "img/xicon.svg";
 }
 
