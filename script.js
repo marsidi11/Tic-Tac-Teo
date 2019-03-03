c6fcb257ae9a1682619660a13e775094360697fd
Marsid Zyberi <39383860+marsidi11@users.noreply.github.com>
2019-03-03 13:26:33 +0100

    setInterval => setTimeout

--- script.js
+++ script.js
@@ -133,33 +133,64 @@ function playa() {
 function computer() {
 
 	if (document.getElementById('img7').src == "data:image/gif;base64,R0lGODlhAQABAAD/ACwAAAAAAQABAAACADs") {
-		setInterval(check7, 500) 
+		setTimeout(check7, 500) 
 	}
 	else if (document.getElementById('img2').src == "data:image/gif;base64,R0lGODlhAQABAAD/ACwAAAAAAQABAAACADs") {
-		setInterval(check2, 500)
+		setTimeout(check2, 500)
 	}
 	else if (document.getElementById('img5').src == "data:image/gif;base64,R0lGODlhAQABAAD/ACwAAAAAAQABAAACADs") {
-		setInterval(check5, 500)
+		setTimeout(check5, 500)
 	}
 	else if (document.getElementById('img8').src == "data:image/gif;base64,R0lGODlhAQABAAD/ACwAAAAAAQABAAACADs") {
-		setInterval(check8, 500)
+		setTimeout(check8, 500)
 	}
 	else if (document.getElementById('img9').src == "data:image/gif;base64,R0lGODlhAQABAAD/ACwAAAAAAQABAAACADs") {
-		setInterval(check9, 500)
+		setTimeout(check9, 500)
 	}
 	else if (document.getElementById('img1').src == "data:image/gif;base64,R0lGODlhAQABAAD/ACwAAAAAAQABAAACADs") {
-		setInterval(check1, 500)
+		setTimeout(check1, 500)
 	}
 	else if (document.getElementById('img4').src == "data:image/gif;base64,R0lGODlhAQABAAD/ACwAAAAAAQABAAACADs") {
-		setInterval(check4, 500)
+		setTimeout(check4, 500)
 	}
 	else if (document.getElementById('img3').src == "data:image/gif;base64,R0lGODlhAQABAAD/ACwAAAAAAQABAAACADs") {
-		setInterval(check3, 500)
+		setTimeout(check3, 500)
 	}
 	else if (document.getElementById('img6').src == "data:image/gif;base64,R0lGODlhAQABAAD/ACwAAAAAAQABAAACADs") {
-		setInterval(check6, 500)
+		setTimeout(check6, 500)
 	}
 }
+
+// Computer Check
+
+function check1() {
+	document.getElementById('img1').src = "img/xicon.svg";
+}
+function check2() {
+	document.getElementById('img2').src = "img/xicon.svg";
+}
+function check3() {
+	document.getElementById('img3').src = "img/xicon.svg";
+}
+function check4() {
+	document.getElementById('img4').src = "img/xicon.svg";
+}
+function check5() {
+	document.getElementById('img5').src = "img/xicon.svg";
+}
+function check6() {
+	document.getElementById('img6').src = "img/xicon.svg";
+}
+function check7() {
+	document.getElementById('img7').src = "img/xicon.svg";
+}
+function check8() {
+	document.getElementById('img8').src = "img/xicon.svg";
+}
+function check9() {
+	document.getElementById('img9').src = "img/xicon.svg";
+}
+
 
 // Table Button Click
 
@@ -206,35 +237,4 @@ function po9() {
 	document.getElementById('img9').src = "img/oicon.svg";
 	computer();
 	winon();
-}
-
-
-// Computer Check
-
-function check1() {
-	document.getElementById('img1').src = "img/xicon.svg";
-}
-function check2() {
-	document.getElementById('img2').src = "img/xicon.svg";
-}
-function check3() {
-	document.getElementById('img3').src = "img/xicon.svg";
-}
-function check4() {
-	document.getElementById('img4').src = "img/xicon.svg";
-}
-function check5() {
-	document.getElementById('img5').src = "img/xicon.svg";
-}
-function check6() {
-	document.getElementById('img6').src = "img/xicon.svg";
-}
-function check7() {
-	document.getElementById('img7').src = "img/xicon.svg";
-}
-function check8() {
-	document.getElementById('img8').src = "img/xicon.svg";
-}
-function check9() {
-	document.getElementById('img9').src = "img/xicon.svg";
 }
