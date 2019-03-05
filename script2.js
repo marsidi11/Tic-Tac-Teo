2e58300d6004a50233cd460014053d459ab3cd00
Marsid Zyberi <39383860+marsidi11@users.noreply.github.com>
2019-03-05 20:05:13 +0100

    2P Work

--- script2.js
+++ script2.js
@@ -1,42 +1,81 @@
 var o = 5;
-// var bs1 = document.getElementById("img1").src;
-// var bs2 = document.getElementById("img2").src;
-// var bs3 = document.getElementById("img3").src;
-// var bs4 = document.getElementById("img4").src;
-// var bs5 = document.getElementById("img5").src;
-// var bs6 = document.getElementById("img6").src;
-// var bs7 = document.getElementById("img7").src;
-// var bs8 = document.getElementById("img8").src;
-// var bs9 = document.getElementById("img9").src;
-// var mo = "img/oicon.svg";
-// var mx = "img/xicon.svg";
-// var m = "data:image/gif;base64,R0lGODlhAQABAAD/ACwAAAAAAQABAAACADs";
 
-function winoon() {
+function wino() {
 	
+	var bs1 = document.getElementById("img1").src;
+	var bs2 = document.getElementById("img2").src;
+	var bs3 = document.getElementById("img3").src;
+	var bs4 = document.getElementById("img4").src;
+	var bs5 = document.getElementById("img5").src;
+	var bs6 = document.getElementById("img6").src;
+	var bs7 = document.getElementById("img7").src;
+	var bs8 = document.getElementById("img8").src;
+	var bs9 = document.getElementById("img9").src;
+	var m = "data:image/gif;base64,R0lGODlhAQABAAD/ACwAAAAAAQABAAACADs";
 
-	// CHECK IF "X" WON
-	
+		if ((bs1 == bs2) && (bs1 == bs3) && (bs1 !== m) && (bs2 !== m) && (bs3 !== m)) {
+			winplayero();
+		} 
+		if ((bs4 == bs5) && (bs4 == bs6) && (bs4 !== m) && (bs5 !== m) && (bs6 !== m)) {
+			winplayero();
+		} 
+		if ((bs7 == bs8) && (bs7 == bs9) && (bs7 !== m) && (bs8 !== m) && (bs9 !== m)) {
+			winplayero();
+		} 
+		if ((bs1 == bs4) && (bs1 == bs7) && (bs1 !== m) && (bs4 !== m) && (bs7 !== m)) {
+			winplayero();
+		} 
+		if ((bs2 == bs5) && (bs2 == bs8) && (bs2 !== m) && (bs5 !== m) && (bs8 !== m)) {
+			winplayero();
+		} 
+		if ((bs3 == bs6) && (bs3 == bs9) && (bs3 !== m) && (bs6 !== m) && (bs9 !== m)) {
+			winplayero();
+		} 
+		if ((bs1 == bs5) && (bs1 == bs9) && (bs1 !== m) && (bs5 !== m) && (bs9 !== m)) {
+			winplayero();
+		} 
+		if ((bs3 == bs5) && (bs3 == bs7) && (bs3 !== m) && (bs5 !== m) && (bs7 !== m)) {
+			winplayero();
+		}
+}
+function winx() {
+	var bs1 = document.getElementById("img1").src;
+	var bs2 = document.getElementById("img2").src;
+	var bs3 = document.getElementById("img3").src;
+	var bs4 = document.getElementById("img4").src;
+	var bs5 = document.getElementById("img5").src;
+	var bs6 = document.getElementById("img6").src;
+	var bs7 = document.getElementById("img7").src;
+	var bs8 = document.getElementById("img8").src;
+	var bs9 = document.getElementById("img9").src;
+	var m = "data:image/gif;base64,R0lGODlhAQABAAD/ACwAAAAAAQABAAACADs";
+
+		if ((bs1 == bs2) && (bs1 == bs3) && (bs1 !== m) && (bs2 !== m) && (bs3 !== m)) {
+			winplayerx();
+		} 
+		if ((bs4 == bs5) && (bs4 == bs6) && (bs4 !== m) && (bs5 !== m) && (bs6 !== m)) {
+			winplayerx();
+		} 
+		if ((bs7 == bs8) && (bs7 == bs9) && (bs7 !== m) && (bs8 !== m) && (bs9 !== m)) {
+			winplayerx();
+		} 
+		if ((bs1 == bs4) && (bs1 == bs7) && (bs1 !== m) && (bs4 !== m) && (bs7 !== m)) {
+			winplayerx();
+		} 
+		if ((bs2 == bs5) && (bs2 == bs8) && (bs2 !== m) && (bs5 !== m) && (bs8 !== m)) {
+			winplayerx();
+		} 
+		if ((bs3 == bs6) && (bs3 == bs9) && (bs3 !== m) && (bs6 !== m) && (bs9 !== m)) {
+			winplayerx();
+		} 
+		if ((bs1 == bs5) && (bs1 == bs9) && (bs1 !== m) && (bs5 !== m) && (bs9 !== m)) {
+			winplayerx();
+		} 
+		if ((bs3 == bs5) && (bs3 == bs7) && (bs3 !== m) && (bs5 !== m) && (bs7 !== m)) {
+			winplayerx();
+		}
 }
 
-// ON Change
-
-function oc1() {
-	if ((document.getElementById("img1").src == "img/oicon.svg") && (document.getElementById("img2").src == "img/oicon.svg") && (document.getElementById("img3").src == "img/oicon.svg")) {
-		alert("Hello World")
-	}
-}
-
-function oc2() {
-	if ((document.getElementById("img1").src == "img/oicon.svg") && (document.getElementById("img2").src == "img/oicon.svg") && (document.getElementById("img3").src == "img/oicon.svg")) {
-		alert("Hello World")
-	}
-}
-function oc3() {
-	if ((document.getElementById("img1").src == "img/oicon.svg") && (document.getElementById("img2").src == "img/oicon.svg") && (document.getElementById("img3").src == "img/oicon.svg")) {
-		alert("Hello World")
-	}
-}
 // Win O, Win X and Draw Function
 
 function winplayero() {
@@ -92,72 +131,90 @@ function checkpo1() {
 	o += 1;
 	if (o % 2 == 0) {
 		document.getElementById("img1").src = "img/oicon.svg";
+		wino();
 	} else {
 		document.getElementById("img1").src = "img/xicon.svg";
+
 	}
 }
 function checkpo2() {
 	o += 1;
 	if (o % 2 == 0) {
 		document.getElementById("img2").src = "img/oicon.svg";
+		wino();
 	} else {
 		document.getElementById("img2").src = "img/xicon.svg";
+		winx();
 	}
 }
 function checkpo3() {
 	o += 1;
 	if (o % 2 == 0) {
 		document.getElementById("img3").src = "img/oicon.svg";
+		wino();
 	} else {
 		document.getElementById("img3").src = "img/xicon.svg";
+		winx();
 	}
 }
 function checkpo4() {
 	o += 1;
 	if (o % 2 == 0) {
 		document.getElementById("img4").src = "img/oicon.svg";
+		wino();
 	} else {
 		document.getElementById("img4").src = "img/xicon.svg";
+		winx();
 	}
 }
 function checkpo5() {
 	o += 1;
 	if (o % 2 == 0) {
 		document.getElementById("img5").src = "img/oicon.svg";
+		wino();
 	} else {
 		document.getElementById("img5").src = "img/xicon.svg";
+		winx();
 	}
 }
 function checkpo6() {
 	o += 1;
 	if (o % 2 == 0) {
 		document.getElementById("img6").src = "img/oicon.svg";
+		wino();
 	} else {
 		document.getElementById("img6").src = "img/xicon.svg";
+		winx();
 	}
 }
 function checkpo7() {
 	o += 1;
 	if (o % 2 == 0) {
 		document.getElementById("img7").src = "img/oicon.svg";
+		wino();
 	} else {
 		document.getElementById("img7").src = "img/xicon.svg";
+		winx();
 	}
 }
 function checkpo8() {
 	o += 1;
 	if (o % 2 == 0) {
 		document.getElementById("img8").src = "img/oicon.svg";
+		wino();
 	} else {
 		document.getElementById("img8").src = "img/xicon.svg";
+		winx();
 	}
 }
 function checkpo9() {
 	o += 1;
 	if (o % 2 == 0) {
 		document.getElementById("img9").src = "img/oicon.svg";
+		wino();
 	} else {
 		document.getElementById("img9").src = "img/xicon.svg";
+		winx();
 	}
 }
 
@@ -166,40 +223,28 @@ function checkpo9() {
 
 function po1() {
 	checkpo1();
-	winoon();
-	oc1();
 }
 function po2() {
 	checkpo2();
-	winoon();
-	oc2();
 }
 function po3() {
 	checkpo3();
-	winoon();
-	oc3();
 }
 function po4() {
 	checkpo4();
-	winoon();
 }
 function po5() {
 	checkpo5();
-	winoon();
 }
 function po6() {
 	checkpo6();
-	winoon();
 }
 function po7() {
 	checkpo7();
-	winoon();
 }
 function po8() {
 	checkpo8();
-	winoon();
 }
 function po9() {
 	checkpo9();
-	winoon();
 }
