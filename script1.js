1b4031d061cfa1df65c2137def2a6c7254c6fafa
Marsid Zyberi <39383860+marsidi11@users.noreply.github.com>
2019-04-11 22:24:27 +0200

    DIfferent Box

--- script1.js
+++ script1.js
@@ -1,6 +1,22 @@
+// Points
 var pointo = 0;
 var pointx = 0;
 var xtrue;
+
+// Function for shuffle
+
+function shuffle(arr) {
+	for (var i = arr.length - 1; i > 0; i--) {
+		var m = Math.floor(Math.random() * (i + 1));
+		[arr[i], arr[m]] = [arr[m], arr[i]];
+	}
+}
+
+// Shuffle number for "check()"
+
+var numbs = [1,2,3,4,5,6,7,8,9]
+shuffle(numbs)
+
 
 
 // Function Win "O"
@@ -60,7 +76,7 @@ function winx() {
 
 
 	if ((bs1 == bs2) && (bs1 == bs3) && (bs1 !== m) && (bs2 !== m) && (bs3 !== m)) {
-	winplayerx();
+		winplayerx();
 	} 
 	if ((bs4 == bs5) && (bs4 == bs6) && (bs4 !== m) && (bs5 !== m) && (bs6 !== 	m)) {
 		winplayerx();
@@ -164,79 +180,96 @@ function playa() {
 	document.getElementById('img8').src = "data:image/gif;base64,R0lGODlhAQABAAD/ACwAAAAAAQABAAACADs";
 
 	document.getElementById('img9').src = "data:image/gif;base64,R0lGODlhAQABAAD/ACwAAAAAAQABAAACADs";
+	
+
+	// Shuffle Numbers Againg
+
+	shuffle(numbs)
 }
 
+// Shuffled numbers
 
+var r1 = numbs[0];
+var r2 = numbs[1];
+var r3 = numbs[2];
+var r4 = numbs[3];
+var r5 = numbs[4];
+var r6 = numbs[5];
+var r7 = numbs[6];
+var r8 = numbs[7];
+var r9 = numbs[8];
+
+console.log(r1)
 
 // Computer Turn
 
 function computer() {
 
-	if (document.getElementById('img7').src == "data:image/gif;base64,R0lGODlhAQABAAD/ACwAAAAAAQABAAACADs") {
-		setTimeout(check7, 500)
+	if (document.getElementById('img' + r1).src == "data:image/gif;base64,R0lGODlhAQABAAD/ACwAAAAAAQABAAACADs") {
+		setTimeout(check1, 200)
 	}
-	else if (document.getElementById('img2').src == "data:image/gif;base64,R0lGODlhAQABAAD/ACwAAAAAAQABAAACADs") {
-		setTimeout(check2, 500)
+	else if (document.getElementById('img' + r2).src == "data:image/gif;base64,R0lGODlhAQABAAD/ACwAAAAAAQABAAACADs") {
+		setTimeout(check2, 200)
 	}
-	else if (document.getElementById('img5').src == "data:image/gif;base64,R0lGODlhAQABAAD/ACwAAAAAAQABAAACADs") {
-		setTimeout(check5, 500)
+	else if (document.getElementById('img' + r3).src == "data:image/gif;base64,R0lGODlhAQABAAD/ACwAAAAAAQABAAACADs") {
+		setTimeout(check3, 200)
 	}
-	else if (document.getElementById('img8').src == "data:image/gif;base64,R0lGODlhAQABAAD/ACwAAAAAAQABAAACADs") {
-		setTimeout(check8, 500)
+	else if (document.getElementById('img' + r4).src == "data:image/gif;base64,R0lGODlhAQABAAD/ACwAAAAAAQABAAACADs") {
+		setTimeout(check4, 200)
 	}
-	else if (document.getElementById('img9').src == "data:image/gif;base64,R0lGODlhAQABAAD/ACwAAAAAAQABAAACADs") {
-		setTimeout(check9, 500)
+	else if (document.getElementById('img' + r5).src == "data:image/gif;base64,R0lGODlhAQABAAD/ACwAAAAAAQABAAACADs") {
+		setTimeout(check5, 200)
 	}
-	else if (document.getElementById('img1').src == "data:image/gif;base64,R0lGODlhAQABAAD/ACwAAAAAAQABAAACADs") {
-		setTimeout(check1, 500)
+	else if (document.getElementById('img' + r6).src == "data:image/gif;base64,R0lGODlhAQABAAD/ACwAAAAAAQABAAACADs") {
+		setTimeout(check6, 200)
 	}
-	else if (document.getElementById('img4').src == "data:image/gif;base64,R0lGODlhAQABAAD/ACwAAAAAAQABAAACADs") {
-		setTimeout(check4, 500)
+	else if (document.getElementById('img' + r7).src == "data:image/gif;base64,R0lGODlhAQABAAD/ACwAAAAAAQABAAACADs") {
+		setTimeout(check7, 200)
 	}
-	else if (document.getElementById('img3').src == "data:image/gif;base64,R0lGODlhAQABAAD/ACwAAAAAAQABAAACADs") {
-		setTimeout(check3, 500)
+	else if (document.getElementById('img' + r8).src == "data:image/gif;base64,R0lGODlhAQABAAD/ACwAAAAAAQABAAACADs") {
+		setTimeout(check8, 200)
 	}
-	else if (document.getElementById('img6').src == "data:image/gif;base64,R0lGODlhAQABAAD/ACwAAAAAAQABAAACADs") {
-		setTimeout(check6, 500)
+	else if (document.getElementById('img' + r9).src == "data:image/gif;base64,R0lGODlhAQABAAD/ACwAAAAAAQABAAACADs") {
+		setTimeout(check9, 200)
 	}
 }
 
 // Computer Check
 
 function check1() {
-	document.getElementById('img1').src = "img/xicon.svg";
+	document.getElementById('img' + r1).src = "img/xicon.svg";
 	winx();
 }
 function check2() {
-	document.getElementById('img2').src = "img/xicon.svg";
+	document.getElementById('img' + r2).src = "img/xicon.svg";
 	winx();
 }
 function check3() {
-	document.getElementById('img3').src = "img/xicon.svg";
+	document.getElementById('img' + r3).src = "img/xicon.svg";
 	winx();
 }
 function check4() {
-	document.getElementById('img4').src = "img/xicon.svg";
+	document.getElementById('img' + r4).src = "img/xicon.svg";
 	winx();
 }
 function check5() {
-	document.getElementById('img5').src = "img/xicon.svg";
+	document.getElementById('img' + r5).src = "img/xicon.svg";
 	winx();
 }
 function check6() {
-	document.getElementById('img6').src = "img/xicon.svg";
+	document.getElementById('img' + r6).src = "img/xicon.svg";
 	winx();
 }
 function check7() {
-	document.getElementById('img7').src = "img/xicon.svg";
+	document.getElementById('img' + r7).src = "img/xicon.svg";
 	winx();
 }
 function check8() {
-	document.getElementById('img8').src = "img/xicon.svg";
+	document.getElementById('img' + r8).src = "img/xicon.svg";
 	winx();
 }
 function check9() {
-	document.getElementById('img9').src = "img/xicon.svg";
+	document.getElementById('img' + r9).src = "img/xicon.svg";
 	winx();
 }
 
@@ -270,6 +303,8 @@ function po4() {
 function po5() {
 	document.getElementById('img5').src = "img/oicon.svg";
 	computer();
+	wino();
+	windraw();
 }
 function po6() {
 	document.getElementById('img6').src = "img/oicon.svg";
@@ -295,3 +330,18 @@ function po9() {
 	wino();
 	windraw();
 }
+
+
+// Try
+
+/*function funs1() {
+	console.log("Hello 1")
+}
+
+function funs2() {
+	console.log("Hello 2")
+}
+
+var arr = [funs1, funs2]
+
+var thef = arr[0]()*/
